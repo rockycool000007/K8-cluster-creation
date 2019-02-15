ec9be15ac0771297b261c3327410e3a601304600
rockycool000007 <44780407+rockycool000007@users.noreply.github.com>
2019-02-15 13:56:50 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -31,6 +31,7 @@ sudo cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
+sudo apt-key update -y
 sudo apt-get update -y
 
 sudo apt-get install -y  kubelet kubeadm kubectl kubernetes-cni nfs-common
